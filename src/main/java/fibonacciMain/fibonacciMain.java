483bcb2c596bcfb012aea26aa89365212574d7bd
Asuncg <118668178+Asuncg@users.noreply.github.com>
2022-12-16 12:42:05 +0100

    Initial commit

---
+++ src/main/java/fibonacciMain/fibonacciMain.java
@@ -0,0 +1,14 @@
+package fibonacciMain;
+
+import es.iessoterhernandez.daw.endes.fibonacci.Fibonacci;
+
+public class fibonacciMain {
+
+	  public static void main(String[] args) {
+
+		  Fibonacci f = new Fibonacci();
+		  
+		  f.fibonacci();
+		  
+	  }
+}
